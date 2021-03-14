d0dbdc33f8c4fdc4985fdab0c93995ef806a1054
Nick Brook <nrbrook@gmail.com>
2021-03-14 19:36:48 +0000

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = "4.4.0-b.2"
+let checksum = "617d7bab3eb80269f0d72bfdd31b084bb7e07a9993df32101df43f3ce8d2aaed"
+
+let package = Package(
+    name: "AirTurnUI",
+    platforms: [.iOS(.v10), .macOS(.v10.14)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "AirTurnUI",
+            targets: ["AirTurnUITargets"]
+        ),
+    ],
+    dependencies: [
+        .package(name: "AirTurnInterface",
+                         url: "https://github.com/AirTurn/AirTurnInterface-dynamic-SP",
+                         from: Version(version)
+        )
+    ],
+    targets: [
+        .binaryTarget(name: "AirTurnUI", url: "https://airturn.com/framework/AirTurnUI.\(version).dynamic.spm.zip", checksum: checksum),
+        .target(name: "AirTurnUITargets",
+                        dependencies: [
+                            .target(name: "AirTurnUI"),
+                            .product(name: "AirTurnInterface", package: "AirTurnInterface")
+                        ],
+                        path: "Sources"
+        )
+    ]
+)
